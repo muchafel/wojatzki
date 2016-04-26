ac6478606f5c564924113043c14aec4c6fafddc2
Michael Wojatzki <michael.wojatzki@googlemail.com>
2016-04-26 16:03:52 +0200

    some inspection abilities

--- stanceBased_annotationStudy/src/main/java/annotationStudy/evaluation/InspectStanceDistributions.java
+++ stanceBased_annotationStudy/src/main/java/annotationStudy/evaluation/InspectStanceDistributions.java
@@ -1,6 +1,11 @@
 package annotationStudy.evaluation;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -24,10 +29,10 @@ import de.tudarmstadt.ukp.dkpro.core.api.resources.DkproContext;
 import de.tudarmstadt.ukp.dkpro.core.io.bincas.BinaryCasReader;
 
 /**
- * class for inspection the class distributions of 
- * a) FAVOR,AGAINST,NONE 
- * b) all used sub target-stance pairs (combined into usage patterns)
- * class has filtering functions for irony and understandability
+ * class for inspection the class distributions of a) FAVOR,AGAINST,NONE b) all
+ * used sub target-stance pairs (combined into usage patterns) class has
+ * filtering functions for irony and understandability
+ * 
  * @author michael
  *
  */
@@ -36,50 +41,87 @@ public class InspectStanceDistributions {
 	private ArrayList<String> subTargets = new ArrayList<String>(Arrays.asList("secularism", "Same-sex marriage",
 			"religious_freedom", "Conservative_Movement", "Freethinking", "Islam", "No_evidence_for_religion", "USA",
 			"Supernatural_Power_Being", "Life_after_death", "Christianity"));
-	
+
 	public static void main(String[] args) throws IOException, ResourceInitializationException {
 		InspectStanceDistributions inspection = new InspectStanceDistributions();
 		String baseDir = DkproContext.getContext().getWorkspace().getAbsolutePath();
-		inspection.inspectDistributionForMainTarget(baseDir + "/semevalTask6/annotationStudy/curatedTweets/Atheism/all");
-//		inspection.inspectPatterns(baseDir + "/semevalTask6/annotationStudy/curatedTweets/Atheism/all");
+//		inspection
+//				.inspectDistributionForMainTarget(baseDir + "/semevalTask6/annotationStudy/curatedTweets/Atheism/all");
+//		inspection
+//				.inspectDistributionForSubTargets(baseDir + "/semevalTask6/annotationStudy/curatedTweets/Atheism/all");
+		 inspection.inspectPatterns(baseDir +
+		 "/semevalTask6/annotationStudy/curatedTweets/Atheism/all");
 	}
 
-	private void inspectPatterns(String location) throws ResourceInitializationException{
+	private void inspectDistributionForSubTargets(String location) throws ResourceInitializationException {
+		Map<String,FrequencyDistribution<String>> targetToFd= new HashMap<>();
+		for (String subTarget : subTargets) {
+			targetToFd.put(subTarget, new FrequencyDistribution<>());
+		}
+
+		CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(BinaryCasReader.class,
+				BinaryCasReader.PARAM_SOURCE_LOCATION, location, BinaryCasReader.PARAM_PATTERNS, "*.bin",
+				BinaryCasReader.PARAM_TYPE_SYSTEM_LOCATION, "typesystem.bin");
+
+		for (JCas jcas : new JCasIterable(reader)) {
+
+			if (JCasUtil.select(jcas, CuratedIrony.class).isEmpty()
+					&& JCasUtil.select(jcas, CuratedUnderstandability.class).isEmpty()) {
+				for (String subTarget : subTargets) {
+					for (CuratedSubTarget target : JCasUtil.select(jcas, CuratedSubTarget.class)) {
+						if (target.getTarget().equals(subTarget)) {
+							targetToFd.get(subTarget).inc(target.getTarget()+"_"+target.getPolarity());
+						}
+					}
+				}
+			} else {
+			}
+
+		}
+		for(String tar: targetToFd.keySet()){
+			for (String polarity : targetToFd.get(tar).getMostFrequentSamples(3)) {
+				System.out.println(polarity + " " + targetToFd.get(tar).getCount(polarity));
+			}
+		}
+	}
+
+	private void inspectPatterns(String location) throws ResourceInitializationException {
 		CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(BinaryCasReader.class,
 				BinaryCasReader.PARAM_SOURCE_LOCATION, location, BinaryCasReader.PARAM_PATTERNS, "*.bin",
 				BinaryCasReader.PARAM_TYPE_SYSTEM_LOCATION, "typesystem.bin");
 		FrequencyDistribution<String> fd = new FrequencyDistribution<>();
 		for (JCas jcas : new JCasIterable(reader)) {
-			//ignore irony and ununderstandability
-			if (JCasUtil.select(jcas, CuratedIrony.class).isEmpty() && JCasUtil.select(jcas, CuratedUnderstandability.class).isEmpty()) {
-				
-				//ignore NONE
-				if(!JCasUtil.selectSingle(jcas, CuratedMainTarget.class).getPolarity().equals("NONE")){
-					String targetString=getTargetString(jcas);
+			// ignore irony and ununderstandability
+			if (JCasUtil.select(jcas, CuratedIrony.class).isEmpty()
+					&& JCasUtil.select(jcas, CuratedUnderstandability.class).isEmpty()) {
+
+				// ignore NONE
+//				if (!JCasUtil.selectSingle(jcas, CuratedMainTarget.class).getPolarity().equals("NONE")) {
+					String targetString = getTargetString(jcas);
 					fd.inc(targetString);
-				}
+//				}
 			}
 		}
+		System.out.println(fd.getN());
 		for (String pattern : fd.getMostFrequentSamples(fd.getKeys().size())) {
 			System.out.println(pattern + " " + fd.getCount(pattern));
 		}
-		
+
 	}
 
 	private String getTargetString(JCas jcas) {
-		String targetString= "Atheism:"+JCasUtil.selectSingle(jcas, CuratedMainTarget.class).getPolarity();
-		for(String subTarget:subTargets){
-			for(CuratedSubTarget target: JCasUtil.select(jcas,CuratedSubTarget.class)){
-				if(target.getTarget().equals(subTarget) && !target.getPolarity().equals("NONE")){
-					targetString+="_"+target.getTarget()+":"+target.getPolarity();
+		String targetString = "Atheism:" + JCasUtil.selectSingle(jcas, CuratedMainTarget.class).getPolarity();
+		for (String subTarget : subTargets) {
+			for (CuratedSubTarget target : JCasUtil.select(jcas, CuratedSubTarget.class)) {
+				if (target.getTarget().equals(subTarget)  && !target.getPolarity().equals("NONE")) {
+					targetString += "_" + target.getTarget() + ":" + target.getPolarity();
 				}
 			}
 		}
 		return targetString;
 	}
 
-	private void inspectDistributionForMainTarget(String location)
-			throws ResourceInitializationException {
+	private void inspectDistributionForMainTarget(String location) throws ResourceInitializationException {
 		FrequencyDistribution<String> fd = new FrequencyDistribution<>();
 		FrequencyDistribution<String> fd2 = new FrequencyDistribution<>();
 		FrequencyDistribution<String> fd3 = new FrequencyDistribution<>();
@@ -98,29 +140,43 @@ public class InspectStanceDistributions {
 			if (JCasUtil.select(jcas, CuratedUnderstandability.class).isEmpty()) {
 				fd3.inc(JCasUtil.selectSingle(jcas, CuratedMainTarget.class).getPolarity());
 			}
-			if (JCasUtil.select(jcas, CuratedIrony.class).isEmpty() && JCasUtil.select(jcas, CuratedUnderstandability.class).isEmpty()) {
+			if (JCasUtil.select(jcas, CuratedIrony.class).isEmpty()
+					&& JCasUtil.select(jcas, CuratedUnderstandability.class).isEmpty()) {
 				fd4.inc(JCasUtil.selectSingle(jcas, CuratedMainTarget.class).getPolarity());
-			}else{
-				//display tweets that are excluded
+			} else {
+				// display tweets that are excluded
 				System.out.println(JCasUtil.selectSingle(jcas, DocumentMetaData.class).getDocumentId());
 			}
 
 		}
-		System.out.println("unfiltered "+ fd.getN());
+		System.out.println("unfiltered " + fd.getN());
 		for (String polarity : fd.getMostFrequentSamples(3)) {
 			System.out.println(polarity + " " + fd.getCount(polarity));
 		}
-		System.out.println("wo irony "+ fd2.getN());
+		System.out.println("wo irony " + fd2.getN());
 		for (String polarity : fd2.getMostFrequentSamples(3)) {
 			System.out.println(polarity + " " + fd2.getCount(polarity));
 		}
-		System.out.println("wo understandability "+ fd3.getN());
+		System.out.println("wo understandability " + fd3.getN());
 		for (String polarity : fd3.getMostFrequentSamples(3)) {
 			System.out.println(polarity + " " + fd3.getCount(polarity));
 		}
-		System.out.println("wo both "+ fd4.getN());
+		System.out.println("wo both " + fd4.getN());
 		for (String polarity : fd4.getMostFrequentSamples(3)) {
 			System.out.println(polarity + " " + fd4.getCount(polarity));
+		}
+
+	}
+
+	private static void writeToFile(String toPrint, double percentageAgreement, double fleissKappa, String path)
+			throws UnsupportedEncodingException, FileNotFoundException, IOException {
+		BufferedWriter out = new BufferedWriter(
+				new OutputStreamWriter(new FileOutputStream(path + "/agreement.csv", true), "UTF-8"));
+		try {
+			out.write(toPrint + ";" + String.valueOf(percentageAgreement) + ";" + fleissKappa + ""
+					+ System.lineSeparator());
+		} finally {
+			out.close();
 		}
 
 	}
--- stanceBased_annotationStudy/src/main/java/annotationStudy/evaluation/InterAnnotatorAgreement.java
+++ stanceBased_annotationStudy/src/main/java/annotationStudy/evaluation/InterAnnotatorAgreement.java
@@ -1,6 +1,11 @@
 package annotationStudy.evaluation;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,6 +20,7 @@ import org.dkpro.statistics.agreement.coding.CodingAnnotationStudy;
 import org.dkpro.statistics.agreement.coding.FleissKappaAgreement;
 import org.dkpro.statistics.agreement.coding.PercentageAgreement;
 
+import consolidatedTypes.MainTarget;
 import consolidatedTypes.SubTarget;
 import curatedTypes.CuratedIrony;
 import curatedTypes.CuratedMainTarget;
@@ -30,15 +36,19 @@ public class InterAnnotatorAgreement {
 	private ArrayList<String> subTargets = new ArrayList<String>(Arrays.asList("secularism", "Same-sex marriage",
 			"religious_freedom", "Conservative_Movement", "Freethinking", "Islam", "No_evidence_for_religion", "USA",
 			"Supernatural_Power_Being", "Life_after_death", "Christianity"));
-	
+
+	private static boolean useSubTargets = false;
+
 	private ArrayList<String> annotators = new ArrayList<String>(
 			Arrays.asList("DominikLawatsch", "NiklasMeyer", "michael_the_annotator"));
 
 	public static void main(String[] args) throws Exception {
 		String baseDir = DkproContext.getContext().getWorkspace().getAbsolutePath();
 		InterAnnotatorAgreement agreementCalculator = new InterAnnotatorAgreement();
-//		agreementCalculator.calculateIAA(baseDir + "/semevalTask6/annotationStudy/curatedTweets/Atheism/all");
-		agreementCalculator.calculateTotalIAA(baseDir + "/semevalTask6/annotationStudy/curatedTweets/Atheism/all");
+		agreementCalculator.calculateIAA(baseDir + "/semevalTask6/annotationStudy/curatedTweets/Atheism/all",
+				useSubTargets);
+		// agreementCalculator.calculateTotalIAA(baseDir +
+		// "/semevalTask6/annotationStudy/curatedTweets/Atheism/all");
 	}
 
 	private void calculateTotalIAA(String path) throws Exception {
@@ -51,49 +61,97 @@ public class InterAnnotatorAgreement {
 		if (annotators.size() > 3) {
 			throw new Exception("unhandled number of annotators");
 		}
-		
+
 		for (JCas jcas : new JCasIterable(reader)) {
 			// ignore irony and ununderstandability
-			if (JCasUtil.select(jcas, CuratedIrony.class).isEmpty()&& JCasUtil.select(jcas, CuratedUnderstandability.class).isEmpty()) {
+			if (JCasUtil.select(jcas, CuratedIrony.class).isEmpty()
+					&& JCasUtil.select(jcas, CuratedUnderstandability.class).isEmpty()) {
 				List<String> decisions = new ArrayList<>();
-				for(String annotator: annotators){
-					decisions.add(getAnnotatorDecisison(jcas,annotator));
+				for (String annotator : annotators) {
+					decisions.add(getAnnotatorDecisison(jcas, annotator));
 				}
 				fd.incAll(decisions);
-				study.addItem(decisions.get(0),decisions.get(1),decisions.get(2));
+				study.addItem(decisions.get(0), decisions.get(1), decisions.get(2));
 			}
 		}
-//		System.out.println(target);
+		// System.out.println(target);
 		for (String polarity : fd.getMostFrequentSamples(fd.getKeys().size())) {
 			System.out.println(polarity + " " + fd.getCount(polarity));
 		}
 		PercentageAgreement pa = new PercentageAgreement(study);
 		FleissKappaAgreement fleissKappa = new FleissKappaAgreement(study);
-		System.out.println("pa: "+pa.calculateAgreement()+ " fleiss "+fleissKappa.calculateAgreement());
-		
+		System.out.println("pa: " + pa.calculateAgreement() + " fleiss " + fleissKappa.calculateAgreement());
+
 	}
 
 	private String getAnnotatorDecisison(JCas jcas, String annotator) {
-		String targetString= "Atheism:"+JCasUtil.selectSingle(jcas, CuratedMainTarget.class).getPolarity();
-		for(String subTarget:subTargets){
-			for(SubTarget target: JCasUtil.select(jcas,SubTarget.class)){
-				if(target.getAnnotator().equals(annotator) && target.getTarget().equals(subTarget) && !target.getPolarity().equals("NONE")){
-					targetString+="_"+target.getTarget()+":"+target.getPolarity();
+		String targetString = "Atheism:" + JCasUtil.selectSingle(jcas, CuratedMainTarget.class).getPolarity();
+		for (String subTarget : subTargets) {
+			for (SubTarget target : JCasUtil.select(jcas, SubTarget.class)) {
+				if (target.getAnnotator().equals(annotator) && target.getTarget().equals(subTarget)
+						&& !target.getPolarity().equals("NONE")) {
+					targetString += "_" + target.getTarget() + ":" + target.getPolarity();
 				}
 			}
 		}
 		return targetString;
 	}
 
-	private void calculateIAA(String path) throws Exception {
-		
-		for(String target : subTargets){
-			System.out.println(target);
-			System.out.println(getAgreement(path,target));
-		}
-		
-		
+	private void calculateIAA(String path, boolean useSubTargets) throws Exception {
 
+		if (useSubTargets) {
+			for (String target : subTargets) {
+				System.out.println(target);
+				System.out.println(getAgreement(path, target));
+				writeToFile(target + "\t" + getAgreement(path, target), path);
+			}
+		} else {
+			System.out.println(getAgreementMain(path));
+		}
+	}
+
+	private String getAgreementMain(String path) throws Exception {
+		CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(BinaryCasReader.class,
+				BinaryCasReader.PARAM_SOURCE_LOCATION, path, BinaryCasReader.PARAM_PATTERNS, "*.bin",
+				BinaryCasReader.PARAM_TYPE_SYSTEM_LOCATION, "typesystem.bin");
+
+		CodingAnnotationStudy study = new CodingAnnotationStudy(annotators.size());
+		FrequencyDistribution<String> fd = new FrequencyDistribution<>();
+		if (annotators.size() > 3) {
+			throw new Exception("unhandled number of annotators");
+		}
+
+		for (JCas jcas : new JCasIterable(reader)) {
+			// ignore irony and ununderstandability
+			if (JCasUtil.select(jcas, CuratedIrony.class).isEmpty()
+					&& JCasUtil.select(jcas, CuratedUnderstandability.class).isEmpty()) {
+				List<String> decisions = new ArrayList<>();
+				for (String annotator : annotators) {
+					decisions.add(getAnnotatorDecisisonMain(jcas, annotator));
+				}
+				fd.incAll(decisions);
+				study.addItem(decisions.get(0), decisions.get(1), decisions.get(2));
+			}
+		}
+		PercentageAgreement pa = new PercentageAgreement(study);
+		FleissKappaAgreement fleissKappa = new FleissKappaAgreement(study);
+		
+		String toPrint = String.valueOf(fd.getCount("ATHEISM_FAVOR")) + "\t"
+				+ String.valueOf(fd.getCount("ATHEISM_AGAINST")) + "\t"
+				+ String.valueOf(fd.getCount("ATHEISM_NONE"));
+
+		return toPrint+"\t"+pa.calculateAgreement() + "\t" + fleissKappa.calculateAgreement();
+	}
+
+	private String getAnnotatorDecisisonMain(JCas jcas, String annotator) {
+		for (MainTarget target : JCasUtil.select(jcas, MainTarget.class)) {
+			if (target.getAnnotator().equals(annotator)) {
+				System.out.println(
+						"ATHEISM_" + target.getAnnotator() + " " + target.getPolarity() + " " + target.getTarget());
+				return "ATHEISM_" + target.getPolarity();
+			}
+		}
+		return "ATHEISM_NONE";
 	}
 
 	private String getAgreement(String path, String target) throws Exception {
@@ -106,37 +164,53 @@ public class InterAnnotatorAgreement {
 		if (annotators.size() > 3) {
 			throw new Exception("unhandled number of annotators");
 		}
-		
+
 		for (JCas jcas : new JCasIterable(reader)) {
 			// ignore irony and ununderstandability
-			if (JCasUtil.select(jcas, CuratedIrony.class).isEmpty()&& JCasUtil.select(jcas, CuratedUnderstandability.class).isEmpty()) {
+			if (JCasUtil.select(jcas, CuratedIrony.class).isEmpty()
+					&& JCasUtil.select(jcas, CuratedUnderstandability.class).isEmpty()) {
 				List<String> decisions = new ArrayList<>();
-				for(String annotator: annotators){
-					decisions.add(getAnnotatorDecisison(jcas,target,annotator));
+				for (String annotator : annotators) {
+					decisions.add(getAnnotatorDecisison(jcas, target, annotator));
 				}
 				fd.incAll(decisions);
-				study.addItem(decisions.get(0),decisions.get(1),decisions.get(2));
+				study.addItem(decisions.get(0), decisions.get(1), decisions.get(2));
 			}
 		}
-//		System.out.println(target);
-//		for (String polarity : fd.getMostFrequentSamples(3)) {
-//			System.out.println(polarity + " " + fd.getCount(polarity));
-//		}
+		// System.out.println(target);
+		// for (String polarity : fd.getMostFrequentSamples(3)) {
+		// System.out.println(polarity + " " + fd.getCount(polarity));
+		// }
 		PercentageAgreement pa = new PercentageAgreement(study);
 		FleissKappaAgreement fleissKappa = new FleissKappaAgreement(study);
-//		System.out.println("pa: "+pa.calculateAgreement()+ " fleiss "+fleissKappa.calculateAgreement());
-		String toPrint=String.valueOf(fd.getCount(target+"_FAVOR"))+"\t"+String.valueOf(fd.getCount(target+"AGAINST"))+"\t"+String.valueOf(fd.getCount(target+"_NONE"));
-		return toPrint+"\t"+pa.calculateAgreement()+"\t"+fleissKappa.calculateAgreement();
+		// System.out.println("pa: "+pa.calculateAgreement()+ " fleiss
+		// "+fleissKappa.calculateAgreement());
+		String toPrint = String.valueOf(fd.getCount(target + "_FAVOR")) + "\t"
+				+ String.valueOf(fd.getCount(target + "AGAINST")) + "\t"
+				+ String.valueOf(fd.getCount(target + "_NONE"));
+		return toPrint + "\t" + pa.calculateAgreement() + "\t" + fleissKappa.calculateAgreement();
 	}
 
 	private String getAnnotatorDecisison(JCas jcas, String target, String annotator) {
-		for(SubTarget subtarget: JCasUtil.select(jcas, SubTarget.class)){
-			if(subtarget.getTarget().equals(target) && subtarget.getAnnotator().equals(annotator)){
-//				System.out.println(annotator+": "+subtarget.getTarget()+"_"+subtarget.getPolarity());
-				return subtarget.getTarget()+"_"+subtarget.getPolarity();
+		for (SubTarget subtarget : JCasUtil.select(jcas, SubTarget.class)) {
+			if (subtarget.getTarget().equals(target) && subtarget.getAnnotator().equals(annotator)) {
+				// System.out.println(annotator+":
+				// "+subtarget.getTarget()+"_"+subtarget.getPolarity());
+				return subtarget.getTarget() + "_" + subtarget.getPolarity();
 			}
 		}
-		return target+"_NONE";
+		return target + "_NONE";
 	}
 
+	private static void writeToFile(String toPrint, String path)
+			throws UnsupportedEncodingException, FileNotFoundException, IOException {
+		BufferedWriter out = new BufferedWriter(
+				new OutputStreamWriter(new FileOutputStream(path + "/agreement.csv", true), "UTF-8"));
+		try {
+			out.write(toPrint + System.lineSeparator());
+		} finally {
+			out.close();
+		}
+
+	}
 }
