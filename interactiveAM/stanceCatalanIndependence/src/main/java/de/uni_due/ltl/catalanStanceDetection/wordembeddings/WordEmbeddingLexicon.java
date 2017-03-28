64a6ef4742ec90e3958030e498dd082a287c0ead
Michael Wojatzki <michael.wojatzki@googlemail.com>
2017-03-28 09:46:36 +0200

    updates based on experiment (embedding stuff, skeleton for LSTM)

---
+++ interactiveAM/stanceCatalanIndependence/src/main/java/de/uni_due/ltl/catalanStanceDetection/wordembeddings/WordEmbeddingLexicon.java
@@ -0,0 +1,78 @@
+package de.uni_due.ltl.catalanStanceDetection.wordembeddings;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+public class WordEmbeddingLexicon {
+
+	private Map<String, List<Double>> lexicon;
+	
+	
+	public Map<String, List<Double>> getLexicon() {
+		return lexicon;
+	}
+
+	public WordEmbeddingLexicon(Map<String, List<Double>> lexicon){
+		this.lexicon= lexicon;
+	}
+	
+	public WordEmbeddingLexicon(String path) throws Exception{
+		this.lexicon= readLexicon(path);
+	}
+	private Map<String, List<Double>> readLexicon(String path) throws Exception {
+		Map<String, List<Double>> lexicon=new HashMap<String, List<Double>>();
+		try (FileReader fr = new FileReader(path); BufferedReader br = new BufferedReader(fr)) {
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				int i=0;
+				String key="";
+				List<Double> value= new ArrayList<Double>();
+				for(String subString : line.split(" ")){
+					if(i==0)key=subString;
+					else{
+						value.add(Double.parseDouble(subString));
+					}
+					i++;
+				}
+				lexicon.put(key, value);
+			}
+		} catch (Exception e) {
+			throw e;
+		}
+		
+		return lexicon;
+	}
+	public void addEntry(String key,List<Double> value ){
+		this.lexicon.put(key, value);
+	}
+	public List<Double> getEmbedding(String key) {
+		if(lexicon.containsKey(key)){
+			return this.lexicon.get(key);
+		}
+		else {
+			return emptyVector();
+		}
+	}
+	
+	
+	private List<Double> emptyVector() {
+		List<Double> emptyVector=new ArrayList<Double>();
+		for(int i=0; i< getDimensionality(); i++){
+			emptyVector.add(0.0);
+		}
+		return emptyVector;
+	}
+	public int getDimensionality() {
+		return this.lexicon.entrySet().iterator().next().getValue().size();
+	}
+	
+	public Set<String> getKeySet() {
+		return lexicon.keySet();
+	}
+}
