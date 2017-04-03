9453e4c972aef0f3dd5ef75c56c4a1284524318b
Michael Wojatzki <michael.wojatzki@googlemail.com>
2017-04-03 11:49:01 +0200

    initial commit

---
+++ germeval2017/src/main/java/de/germeval2017/udeAtGermeval/io/CreateGermevalData.java
@@ -0,0 +1,12 @@
+package de.germeval2017.udeAtGermeval.io;
+
+import java.io.File;
+
+public class CreateGermevalData {
+
+	public static void main(String[] args) {
+		File input=  new File("");
+
+	}
+
+}
