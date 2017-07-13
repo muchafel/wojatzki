8278886a1148dab1c08e9ca5f7b9f77c5ad79747
Yafei <halloweltyan@gmail.com>
2017-07-13 15:49:21 +0200

    accroding to the width of browser, calculates width of three pie charts on result view

--- interactiveStance/src/main/java/de/uni/due/ltl/interactiveStance/client/ResultView.java
+++ interactiveStance/src/main/java/de/uni/due/ltl/interactiveStance/client/ResultView.java
@@ -3,16 +3,14 @@ package de.uni.due.ltl.interactiveStance.client;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.ClientConnector;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.server.Page;
+import com.vaadin.server.Sizeable;
+import com.vaadin.ui.*;
 
 import de.uni.due.ltl.interactiveStance.backend.BackEnd;
 import de.uni.due.ltl.interactiveStance.backend.EvaluationResult;
 import de.uni.due.ltl.interactiveStance.client.charts.PredictionQualityPieChart;
+import org.vaadin.addon.JFreeChartWrapper;
 
 public class ResultView extends VerticalLayout implements View {
 
@@ -56,9 +54,24 @@ public class ResultView extends VerticalLayout implements View {
 	 */
 	private void addResults(EvaluationResult result) {
 		pieCharts.removeAllComponents();
-		pieCharts.addComponent(new PredictionQualityPieChart().createPieChart("FAVOR", result.getfFAVOR()));
-		pieCharts.addComponent(new PredictionQualityPieChart().createPieChart("AGAINST", result.getfAGAINST()));
-		pieCharts.addComponent(new PredictionQualityPieChart().createPieChart("NONE", result.getfNONE()));
+		float w = (float) (UI.getCurrent().getPage().getBrowserWindowWidth());
+		float h = (w/3.0f) / 1.66f;
+		//780*470
+		JFreeChartWrapper favorChart = new PredictionQualityPieChart().createPieChart("FAVOR", result.getfFAVOR());
+		JFreeChartWrapper againstChart = new PredictionQualityPieChart().createPieChart("AGAINST", result.getfAGAINST());
+		JFreeChartWrapper noneChart = new PredictionQualityPieChart().createPieChart("NONE", result.getfNONE());
+		favorChart.setWidth(w/3, Unit.PIXELS);
+		favorChart.setHeight(h, Unit.PIXELS);
+		againstChart.setWidth(w/3, Unit.PIXELS);
+		againstChart.setHeight(h, Unit.PIXELS);
+		noneChart.setWidth(w/3, Unit.PIXELS);
+		noneChart.setHeight(h, Unit.PIXELS);
+		pieCharts.addComponent(favorChart);
+		pieCharts.addComponent(againstChart);
+		pieCharts.addComponent(noneChart);
+//		pieCharts.addComponent(new PredictionQualityPieChart().createPieChart("FAVOR", result.getfFAVOR()));
+//		pieCharts.addComponent(new PredictionQualityPieChart().createPieChart("AGAINST", result.getfAGAINST()));
+//		pieCharts.addComponent(new PredictionQualityPieChart().createPieChart("NONE", result.getfNONE()));
 		microF1Label.setValue("MICRO F1 "+String.valueOf(result.getMicroF()));
 		macroF1Label.setValue("MACRO F1 "+String.valueOf(result.getMacroF()));
 	}
