07a3cbccc6fa118ffbda01f2758abee8378f566b
Michael Wojatzki <michael.wojatzki@googlemail.com>
2018-06-11 11:01:49 +0200

    clean up commit III

--- germeval2017/src/main/java/de/germeval2017/Evaluation/objectBindings/SentimentAspect.java
+++ germeval2017/src/main/java/de/germeval2017/Evaluation/objectBindings/SentimentAspect.java
@@ -6,6 +6,8 @@ public class SentimentAspect {
 	
 	private int begin;
 	private int end;
+	private int begin2;
+	private int end2;
 	private String aspect;
 	private String ote;
 	private String sentiment;
@@ -32,6 +34,24 @@ public class SentimentAspect {
 	}
 	public void setEnd(int end) {
 		this.end = end;
+	}
+	
+	
+	@XmlAttribute(name = "from2")
+	public int getBegin2() {
+		return begin2;
+	}
+	
+	public void setBegin2(int begin) {
+		this.begin2 = begin;
+	}
+	
+	@XmlAttribute(name = "to2")
+	public int getEnd2() {
+		return end2;
+	}
+	public void setEnd2(int end2) {
+		this.end2 = end2;
 	}
 	
 	@XmlAttribute(name = "category")
